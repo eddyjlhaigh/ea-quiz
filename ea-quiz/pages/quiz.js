8adadfdddd924a1d1b9b2aa18b5f9a2363f24fb4
eddyjlhaigh <eddyhaigh@gmail.com>
2021-01-29 20:47:56 +0000

    Add quiz answers radio buttons

--- ea-quiz/pages/quiz.js
+++ ea-quiz/pages/quiz.js
@@ -15,6 +15,21 @@ export default function Quiz() {
       <div id="question-heading">
         <h1>Question 1</h1>
       </div>
+
+      <div id="answers">
+        <input type="radio" value="" name="answer" /> Answer 1
+        <input type="radio" value="" name="answer" /> Answer 2
+        <input type="radio" value="" name="answer" /> Answer 3
+        <input type="radio" value="" name="answer" /> Answer 4
+      </div>
+
+      <div id="submit-answer">
+        <Link href="results">
+          <button>
+            Submit
+          </button>
+        </Link>
+      </div>
     </>
   )
 }
---
+++ ea-quiz/pages/results.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Results() {
+  return (
+    <>
+      <Link href="/">
+        <a>Back to start</a>
+      </Link>
+
+      <h1>Results</h1>
+    </>
+  )
+}
